45249a7187e9c6a150983622338c2547e5ae2a5d
Abdulloh31 <abdullah.aflaha.aslam@gmail.com>
2022-09-16 11:41:37 +0700

    RESOLV ABDULLAH AFLAHA ASLAM

--- promise.js
+++ promise.js
@@ -1,7 +1,46 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (parameter) => {
+  return new Promise(async (resolve, reject) => {
+
+    //menampung data array
+    const ixx = await promiseTheaterIXX();
+    const vgc = await promiseTheaterVGC();
+    var marah = 0;
+    var tidak_marah = 0;
+
+    //hitung yang amrah di ixx
+    ixx.forEach((value, index) => {
+      if (value.hasil == 'marah') {
+        marah = marah + 1;
+      } else {
+        tidak_marah = tidak_marah + 1;
+      }
+    });
+
+    // hitung yang marah/tidak marah di vgc
+    vgc.forEach((value, index) => {
+      if (value.hasil == 'marah') {
+        marah = marah + 1;
+      } else {
+        tidak_marah = tidak_marah + 1;
+      }
+    });
+    // Kondisi gagal
+    if (parameter === null) {
+      reject('Maaf, katanya Error !');
+    }
+    // Kondisi terpenuhi
+    else {
+      if (parameter == 'marah') {
+        resolve(marah);
+      } else {
+        resolve(tidak_marah);
+      }
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
